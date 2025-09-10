556ee2e703c1844d1b2de2590990700a11321ed0
Azeem <mazeem.ajm@gmail.com>
2025-09-11 02:33:14 +0530

    initial commit

--- app/lib/Firebase.tsx
+++ app/lib/Firebase.tsx
@@ -1,15 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { config } from 'dotenv';
+
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXP3L6fxuPrYw_4k02F9dsLOT6JCmB6Dg",
-  authDomain: "portfolio-next-e6e96.firebaseapp.com",
-  projectId: "portfolio-next-e6e96",
-  storageBucket: "portfolio-next-e6e96.firebasestorage.app",
-  messagingSenderId: "155133702653",
-  appId: "1:155133702653:web:a93f85bd96f117dd076994",
-  measurementId: "G-JDS21NKVDB"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 
